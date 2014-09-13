fe70ac01f51e591b2f3e08ea4056e3864e326f92
tillarnold <throwable42@gmail.com>
2014-09-13 23:49:37 +0200

    Remove unused imports from test.js

--- test/test.js
+++ test/test.js
@@ -1,12 +1,3 @@
-var test = require('tape');
-
-var shapes = require('..');
-var Vector = shapes.Vector;
-var Rect = shapes.Rectangle;
-var Circle = shapes.Circle;
-var Polygon = shapes.Polygon;
-
-
 require('./rectangleTest.js');
 require('./circleTest.js');
 require('./vectorTest.js');

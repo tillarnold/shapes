d553148fa82a7bef3343d905d5b5333ea87fc540
tillarnold <throwable42@gmail.com>
2014-09-13 22:04:28 +0200

    Clean up

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ module.exports.Rectangle = require('./lib/rectangle.js');
 module.exports.Circle = require('./lib/circle.js');
 module.exports.Polygon = require('./lib/polygon.js');
 
-
-
 module.exports.ShapeEventEmitter = require('./lib/shapeEventEmitter.js');

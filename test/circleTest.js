0081aaea9004e56f8ce13dba1ccccba54776c0e0
tillarnold <throwable42@gmail.com>
2014-10-15 11:33:05 +0000

    Add coverage npm script and improve coverage for circle

--- test/circleTest.js
+++ test/circleTest.js
@@ -9,3 +9,14 @@ test('Circle constructor', function(t) {
   var c = new Circle(new Vector(50, 50), 10);
   t.equal(c.radius, 10);
 });
+
+test('Circle#contains', function(t) {
+  t.plan(2);
+  var c = new Circle( [20,20], 10);
+
+  var v1 = [21,18];
+  var v2 = [99,18];
+
+  t.equal(c.contains(v1), true);
+  t.equal(c.contains(v2), false);
+});

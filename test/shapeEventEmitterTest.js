563faaf2a7ee1a1b1174a3d95ca11abcc2f659be
tillarnold <throwable42@gmail.com>
2014-10-15 12:43:36 +0000

    Improve ShapeEventEmitter coverage for mouseout

--- test/shapeEventEmitterTest.js
+++ test/shapeEventEmitterTest.js
@@ -62,18 +62,26 @@ test('ShapeEventEmitter mouseover and mouseout', function(t) {
   var cee = new EventEmitter();
   var see = new ShapeEventEmitter(r,cee);
 
-  t.plan(3);
+  var overs = 0;
+  var outs = 0;
+
+  t.plan(6);
 
   see.on('mouseover',function(e){
      t.equal(e.type, 'mouseover');
+     overs++;
   });
 
   see.on('mouseout',function(e){
     t.equal(e.type, 'mouseout');
+    outs++;
   });
-
 
   cee.emit('mousemove',{x:50,y:50});
   cee.emit('mousemove',{x:500,y:50});
   cee.emit('mousemove',{x:50,y:60});
+  cee.emit('mouseout',{});
+
+  t.equal(overs, 2);
+  t.equal(outs, 2);
 });

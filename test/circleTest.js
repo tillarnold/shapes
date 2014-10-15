a7b6bfcd1deec3cb63bfdfcaebeb17f336a18cec
tillarnold <throwable42@gmail.com>
2014-10-15 11:47:43 +0000

    Improve coverage in polygon and circle

--- test/circleTest.js
+++ test/circleTest.js
@@ -20,3 +20,42 @@ test('Circle#contains', function(t) {
   t.equal(c.contains(v1), true);
   t.equal(c.contains(v2), false);
 });
+
+test('Circle#round', function(t) {
+  t.plan(3);
+  var c = new Circle( [20.49,25.33], 0.76);
+  var c2 = c.round();
+
+  t.equal(c2.center.x, 20);
+  t.equal(c2.center.y, 25);
+  t.equal(c2.radius, 1);
+});
+
+
+test('Circle#clone', function(t) {
+  t.plan(3);
+  var c = new Circle( [55,66],3);
+  var c2 = c.clone();
+
+  c.center.x = 100;
+  c.center.y = 10440;
+  c.radius = 12;
+
+  t.equal(c2.center.x, 55);
+  t.equal(c2.center.y, 66);
+  t.equal(c2.radius, 3);
+});
+
+test('Circle#moveBy', function(t) {
+  t.plan(3);
+  var c = new Circle( [55,66],3);
+  var c2 = c.moveBy([10,20]);
+
+  c.center.x = 100;
+  c.center.y = 10440;
+  c.radius = 12;
+
+  t.equal(c2.center.x, 65);
+  t.equal(c2.center.y, 86);
+  t.equal(c2.radius, 3);
+});
--- test/polygonTest.js
+++ test/polygonTest.js
@@ -33,3 +33,46 @@ test('Polygon#contains', function(t) {
   t.equals(p1.contains([15, 15]), true);
   t.equals(p1.contains([21, 15]), false);
 });
+
+test('Polygon#round', function(t) {
+  t.plan(1);
+
+  var p0 = new Polygon([
+    [10, 10],
+    [10, 20],
+    [20, 20],
+    [20, 10]
+  ]);
+
+  var p1 = new Polygon([
+    [9.5, 9.6],
+    [10.4, 20.3],
+    [19.7, 19.6],
+    [20.333, 9.7]
+  ]);
+
+  var p2 = p1.round();
+  t.deepEqual(p2.vectors, p0.vectors);
+});
+
+
+test('Polygon#round', function(t) {
+  t.plan(1);
+
+  var p0 = new Polygon([
+    [15, 11],
+    [15, 21],
+    [25, 21],
+    [25, 11]
+  ]);
+
+  var p1 = new Polygon([
+    [10, 10],
+    [10, 20],
+    [20, 20],
+    [20, 10]
+  ]);
+
+  var p2 = p1.moveBy([5,1]);
+  t.deepEqual(p2.vectors, p0.vectors);
+});

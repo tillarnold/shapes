8269fac6d04d1466a7f3fbbaabb5d192368e4e98
tillarnold <throwable42@gmail.com>
2014-10-25 21:06:35 +0000

    Improve `ShapeEventEmitter` constructor
    A canvas is now a valid parameter

--- test/manually.js
+++ test/manually.js
@@ -1,9 +1,7 @@
 'use strict';
 
 var shape = require('..');
-//var raf = require('raf');
 
-var CanvasEventEmitter = require('canvas-utils').CanvasEventEmitter;
 var ShapeEventEmitter = shape.ShapeEventEmitter;
 var canvas = document.createElement('canvas');
 var ctx = canvas.getContext('2d');
@@ -11,9 +9,9 @@ var ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
 canvas.style.outline = '1px solid black';
 
-var cee = new CanvasEventEmitter(canvas);
+
 var circle = new shape.Circle([10, 10], 50);
-var see = new ShapeEventEmitter(circle, cee);
+var see = new ShapeEventEmitter(circle, canvas);
 
 
 var colorSwitch = true;

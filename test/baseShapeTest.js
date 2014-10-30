7cd0264ebba7464bbb6ec4018b5ed517ec1076bc
tillarnold <throwable42@gmail.com>
2014-10-30 20:50:11 +0100

    Add `createShapeEventEmitter`

--- test/baseShapeTest.js
+++ test/baseShapeTest.js
@@ -2,8 +2,16 @@
 var test = require('tape');
 
 var BaseShape = require('../lib/baseShape.js');
+var ShapeEventEmitter = require('../').ShapeEventEmitter;
+var Rectangle = require('..').Rectangle;
+
 
 var noop = function() {};
+
+global.HTMLCanvasElement = function HTMLCanvasElement() {};
+global.window = {};
+global.window.document = {};
+global.window.document.addEventListener = noop;
 
 test('BaseShape#clear', function(t) {
   t.plan(2);
@@ -67,4 +75,14 @@ test('BaseShape#stroke', function(t) {
     t.pass('path function was called');
   };
   bs.stroke(mockCtx);
+});
+
+test('BaseShape#createShapeEventEmitter', function(t) {
+  t.plan(2);
+  var canvas = new HTMLCanvasElement();
+  var rect = new Rectangle([10, 10], 50, 50);
+
+  t.equals(rect.createShapeEventEmitter(canvas)._cee, new ShapeEventEmitter(rect, canvas)._cee);
+  t.equals(new ShapeEventEmitter(rect, canvas)._cee, new ShapeEventEmitter(rect, canvas)._cee);
+
 });

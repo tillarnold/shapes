b1ebc1386a98597a9d79d8ab23d05c6b81d29f75
tillarnold <throwable42@gmail.com>
2015-03-07 16:54:11 +0100

    Remove genrated es5
    And update dependencies

--- es5-generated/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports.Vector = require("./lib/vector.js");
-module.exports.Rectangle = require("./lib/rectangle.js");
-module.exports.Circle = require("./lib/circle.js");
-module.exports.Polygon = require("./lib/polygon.js");
-
-module.exports.ShapeEventEmitter = require("./lib/shapeEventEmitter.js");
--- es5-generated/test/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-require("./rectangleTest.js");
-require("./circleTest.js");
-require("./vectorTest.js");
-require("./polygonTest.js");
-require("./baseShapeTest.js");
-require("./shapeEventEmitterTest.js");
